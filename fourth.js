ec89ac8fec13bd174cd0022e7cdf2e8a4356ee21
HentaiSama69 <110540759+HentaiSama69@users.noreply.github.com>
2025-01-28 22:05:45 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,5 @@
+// Function to redirect to page3.html
+function goToNextPage() {
+    window.location.href = "page5.html"; // Replace with the actual path if necessary
+  }
+  
---
+++ second.js
@@ -0,0 +1,5 @@
+// Function to redirect to page3.html
+function goToNextPage() {
+    window.location.href = "page3.html"; // Replace with the actual path if necessary
+  }
+  
